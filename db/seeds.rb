b9f58c0759aaf6f64b8ffcb230dd5f48c699d1db
mikemccomb <mikemccomb@gmail.com>
2023-03-03 13:37:48 -0600

    Add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+song = Song.new(title: "Cha cha cha", album: "", artist: "Kaarija", year: 2023, country: "Finland")
+song.save
+song = Song.new(title: "Stefania", album: "", artist: "Kalush Orchestra", year: 2022, country: "Ukraine")
+song.save
+song = Song.new(title: "ZITTI E BUONI", album: "Rush!", artist: "Maneskin", year: 2021, country: "Italy")
+song.save
+song = Song.new(title: "Arcade", album: "So boring", artist: "Duncan Laurence", year: 2019, country: "Netherlands")
+song.save
