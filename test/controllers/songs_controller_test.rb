93f097503c2723388970c7d91417e5349684eba9
mikemccomb <mikemccomb@gmail.com>
2023-03-03 13:49:00 -0600

    Add index action

---
+++ test/controllers/songs_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SongsControllerTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+  end
+end
